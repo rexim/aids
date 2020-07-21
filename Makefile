7e9268ecc71ff4a79a2ebc24df74c588f38f471a
rexim <reximkut@gmail.com>
2020-07-21 18:34:47 +0700

    Add cat example

--- Makefile
+++
@@ -1,2 +0,0 @@
-aids: main.cpp aids.hpp
-	clang++ -std=c++17 -Wall -fno-exceptions -ggdb -o aids main.cpp
---
+++ examples/cat.cpp
@@ -0,0 +1,25 @@
+#include "aids.hpp"
+
+using namespace aids;
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        println(stderr, "Usage: ", argv[0], " <files...>");
+        exit(1);
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        auto result = read_file_as_string_view(argv[i]);
+        if (!result.has_value) {
+            println(stderr, "Could not read file `", argv[i], "`: ", strerror(errno));
+            exit(1);
+        }
+        auto content = result.unwrap;
+        defer(free((void*) content.data));
+
+        println(stdout, content);
+    }
+
+    return 0;
+}

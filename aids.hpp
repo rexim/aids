5b6f93e3ec5604d2501c35316393d5ca234d4a14
rexim <reximkut@gmail.com>
2020-10-03 11:52:43 +0700

    [0.18.0] Release
    - Rename Args::pop() -> Args::shift()
    - Add more details to Stretchy_Buffer deprecation message

--- aids.hpp
+++ aids.hpp
@@ -21,7 +21,7 @@
 //
 // ============================================================
 //
-// aids — 0.17.0 — std replacement for C++. Designed to aid developers
+// aids — 0.18.0 — std replacement for C++. Designed to aid developers
 // to a better programming experience.
 //
 // https://github.com/rexim/aids
@@ -30,6 +30,8 @@
 //
 // ChangeLog (https://semver.org/ is implied)
 //
+//   0.18.0 Rename Args::pop() -> Args::shift()
+//          Add more details to Stretchy_Buffer deprecation message
 //   0.17.0 Dynamic_Array::concat()
 //          Dynamic_Array::expand_capacity()
 //   0.16.0 Dynamic_Array
@@ -513,9 +515,9 @@ namespace aids
         };
     }
 
-    using Stretchy_Buffer [[deprecated("Use Dynamic_Array instead")]] = deprecated::Stretchy_Buffer;
+    using Stretchy_Buffer [[deprecated("Use Dynamic_Array instead. Stretchy_Buffer is limited to only `char`-s while Dynamic_Array<T> can work with any type T.")]] = deprecated::Stretchy_Buffer;
 
-    [[deprecated("Use Dynamic_Array instead")]]
+    [[deprecated("Use Dynamic_Array instead. Stretchy_Buffer is limited to only `char`-s while Dynamic_Array<T> can work with any type T.")]]
     void print1(FILE *stream, deprecated::Stretchy_Buffer buffer)
     {
         fwrite(buffer.data, 1, buffer.size, stream);
@@ -530,7 +532,13 @@ namespace aids
         int argc;
         char **argv;
 
+        [[deprecated("Use Args::shift() instead. It was decided to rename `pop` to `shift` since it creates confusion with the pop operation of stacks which removes the elements from the other end. And shift is common operation in Bash and Perl (probably others) for parsing command line arguments.")]]
         char *pop()
+        {
+            return shift();
+        }
+
+        char *shift()
         {
             char *result = *argv;
             argv += 1;

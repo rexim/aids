b1f0f21c9097bb1af3c04e9553a038ec1d675476
rexim <reximkut@gmail.com>
2020-11-19 15:40:54 +0700

    Add utf8 encoding/decoding tests

---
+++ tests/utf8_test.cpp
@@ -0,0 +1,41 @@
+#include "../aids.hpp"
+
+using namespace aids;
+
+int main(int argc, char *argv[])
+{
+    const uint32_t UNICODE_LOW = 0x0000;
+    const uint32_t UNICODE_HIGH = 0x10FFFF;
+
+    println(stdout, "Checking decoding/encoding of all code points within the range ",
+            "[0x", HEX<uint32_t> { UNICODE_LOW }, "..0x", HEX<uint32_t> { UNICODE_HIGH }, "]...");
+
+    for (uint32_t expected_code = UNICODE_LOW;
+         expected_code <= UNICODE_HIGH;
+         ++expected_code)
+    {
+        auto uchar = code_to_utf8(expected_code);
+
+        size_t size = 0;
+        auto actual_code = unwrap_or_panic(
+            utf8_get_code(uchar.view(), &size),
+            "TODO: good error messages for failed utf8 decoding");
+
+        if (expected_code != actual_code) {
+            panic("FAILED: ",
+                  "expected code ", HEX<uint32_t> { expected_code }, ", ",
+                  "but got ", HEX<uint32_t> { actual_code }, ".");
+
+        }
+
+        if (size != uchar.count) {
+            panic("FAILED: ",
+                  "expected size of code ", HEX<uint32_t> { expected_code }, " ",
+                  "to be ", uchar.count, ", but it was ", size, ".");
+        }
+    }
+
+    println(stdout, "OK.");
+
+    return 0;
+}

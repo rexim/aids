3418c94e9c3fc11b5420d4a53c7aeb9c1ce04cb9
rexim <reximkut@gmail.com>
2020-12-16 20:51:12 +0700

    [0.31.0] release
    - String_View::has_suffix(String_View suffix)

--- aids.hpp
+++ aids.hpp
@@ -21,7 +21,7 @@
 //
 // ============================================================
 //
-// aids — 0.30.0 — std replacement for C++. Designed to aid developers
+// aids — 0.31.0 — std replacement for C++. Designed to aid developers
 // to a better programming experience.
 //
 // https://github.com/rexim/aids
@@ -30,7 +30,7 @@
 //
 // ChangeLog (https://semver.org/ is implied)
 //
-//          String_View::has_suffix(String_View suffix)
+//   0.31.0 String_View::has_suffix(String_View suffix)
 //   0.30.0 String_View String_View::chop_while(Predicate_Char predicate)
 //   0.29.0 void destroy(Dynamic_Array<T> dynamic_array)
 //   0.28.0 struct Hash_Map

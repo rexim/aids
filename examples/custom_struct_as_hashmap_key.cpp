0023468f9817ae78c9383cd18fd86a688dbe20ea
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2020-12-16 21:44:46 +0100

    Add example: User defined structure as Hash_Map's Key
    This *tutorial on how-to-use Hash_Map* also introduces two prob. TODOs, which only make sense if #8 is resolved and this very commit is merged to the maintrack

---
+++ examples/custom_struct_as_hashmap_key.cpp
@@ -0,0 +1,47 @@
+#include "aids.hpp"
+using aids::operator""_sv;
+
+struct Foo {
+    int i{};
+    aids::String_View sv{};
+};
+
+bool operator==(Foo lhs, Foo rhs) {
+    return ((lhs.i == rhs.i) && (lhs.sv == rhs.sv));
+}
+
+// TODO: operator!=(Key, Key) needs to be implemented in Hash_Map<Key, Value>
+// This time compiler message is quite meaningful (comparing to bug #8)
+// Nevertheless, let's consider checking not only for `hash(Key)` (see #8),
+// but `operator!=(Key, Key)` as well
+bool operator!=(Foo lhs, Foo rhs) {
+    return not (lhs == rhs);
+}
+
+unsigned long hash(int i)
+{
+    return i;
+}
+
+unsigned long hash(Foo foo)
+{
+    //TODO: hash() of custom type needs also bit shifting
+    // This hashing is working, but an official example should comply
+    // with the hashing principles
+    return hash(foo.i) ^ hash(foo.sv);
+}
+
+int main(int argc, char *argv[])
+{
+    aids::Hash_Map<Foo, int> map{};
+    *map[Foo{42, "forty-tooth"_sv}] = 42;
+
+    auto foo1 = Foo{100000, "forty-tooth"_sv};
+    auto foo2 = Foo{42, ""_sv};
+    auto foo3 = Foo{42, "forty-tooth"_sv};
+
+    assert(false == map.get(foo1).has_value);
+    assert(false == map.get(foo2).has_value);
+    assert(true == map.get(foo3).has_value);
+    return 0;
+}
